67dd3f0dc46a5112f4b86d15918c0fa2a47034c7
George Sukhadolsky <sukhadol@yandex.ru>
2021-09-14 13:03:30 +0300

    Update Bot_forKadry.py

--- Bot_forKadry.py
+++ Bot_forKadry.py
@@ -302,15 +302,11 @@ print('....007')
 if __name__ == '__main__':
     if Run_On_Heroku:
         print('....004')
-
-        executor.start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
-        print('....008')
-
-        # def main():
-        #     print('....005')
-        #     #start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
-        #     executor.start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
-        #     print('....006')
+        def main():
+            print('....005')
+            #start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
+            executor.start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
+            print('....006')
     else:
         print('.... 9999')
         executor.start_polling(dp, on_shutdown=shutdown)
