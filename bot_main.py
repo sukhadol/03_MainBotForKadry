dba4a887e816f4fa4dfc0d05503a76234e6bc8dd
George Sukhadolsky <sukhadol@yandex.ru>
2021-09-20 15:52:40 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -46,9 +46,7 @@ if 'We_are_on_Heroku' in os.environ:
 
     async def on_startup(dp):
         await bot.delete_webhook(dp) 
-        print('...все удалено')
-
-        #await bot.set_webhook(WEBHOOK_URL)
+        await bot.set_webhook(WEBHOOK_URL)
         # и дальше все что надо после запуска
 
     async def on_shutdown(dp):
@@ -64,50 +62,6 @@ else:
     dp = Dispatcher(bot, storage=storage)
 
 print('....вводную часть завершили')
-
-#++++++++++++
-
-
-
-
-# #просто повтор сказанного пользователем
-# @dp.message_handler()
-# async def echo(message: types.Message):
-#     await bot.send_message(message.chat.id, message.text)
-
-
-# #===== блок текстов
-
-# help_message = text("здесь текстовое сообщение бла-бла-бла, пока удалено, если вдруг бот не запускается, воспользуйтесь командой /start")
-# @dp.message_handler(lambda message: message.text == btn_help)
-# @dp.message_handler(commands=['help'])
-# async def process_help_command(message: types.Message):
-#     await message.reply(help_message, disable_web_page_preview = True)
-
-# tmp_message = text("здесь потом будет основной функционал общения с пользователем")
-# @dp.message_handler(lambda message: message.text == btn_zapusk)
-# @dp.message_handler(commands=['zapusk'])
-# async def process_help_command(message: types.Message):
-#     await message.reply(tmp_message, disable_web_page_preview = True)
-
-
-# #===== блок Начала
-# btn_zapusk = 'Запуск'
-# btn_help = 'Помощь'
-# MAIN_KB = ReplyKeyboardMarkup(
-#                              resize_keyboard=True).row(
-#                              KeyboardButton(btn_zapusk),
-#                              KeyboardButton(btn_help)
-#                              )
-
-# @dp.message_handler(commands=['start'])
-# async def process_start_command(message: types.Message):
-#     if ((message.from_user.first_name is None) and (message.from_user.first_name is None)):
-#         whom_say = message.from_user.username
-#         await message.answer(f'Привет, @{message.from_user.username}!\n Начинаем работу 👋', reply_markup=MAIN_KB)
-#     else:
-#         whom_say = message.from_user.first_name + ' ' + message.from_user.last_name
-#         await message.answer(f'Привет, {whom_say} (@{message.from_user.username})!\nНачинаем работу 👋', reply_markup=MAIN_KB)
 
 
 #======================== Для работы с состояниями
