9e514477607fbf8824274f01656f73a15ce81e13
George Sukhadolsky <sukhadol@yandex.ru>
2021-10-17 18:47:33 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -314,7 +314,7 @@ async def strange_txt(message: types.Message):
        # await message.answer(text=f' значение поля forward_sender_name :\n\n@{message.forward_from.forward_sender_name}', parse_mode='Markdown')
        # await message.answer(text=f' значение поля full_name :\n\n@{message.forward_from.full_name}', parse_mode='Markdown')
 
-        text_of_FORVARD_obiavy = '*#вакансия от @' + message.forward_from.username + '*\n\n' + message.text 
+        text_of_FORVARD_obiavy = '#вакансия от @' + message.forward_from.username + '\n\n' + message.text 
         await message.answer(text=f'Итого получаем следующий текст:\n\n{text_of_FORVARD_obiavy}', parse_mode='Markdown')
         await Status.st_ADM_02.set()
         await message.answer("Подтверждаете отправку?",
