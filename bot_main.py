caba98c76330b49620940a2b5292dfb8bfc097c1
George Sukhadolsky <sukhadol@yandex.ru>
2021-10-19 19:18:40 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -340,16 +340,16 @@ async def strange_txt(message: types.Message):
 
 
             await message.answer("Hello, <b>world</b>!", parse_mode=types.ParseMode.HTML)
-            await message.answer("Hello со ссылкой, <a href=\"tg://user?id=1111111\">гиперссылка</a>", parse_mode=types.ParseMode.HTML)
-            await message.answer(' html-0 вначале просто <a href=''tg://user?id=1111111''>гиперссылка</a> вот так', parse_mode = types.ParseMode.HTML)
-            await message.answer(' html-1 вначале просто <a href="tg://user?id={UsrInfo.id}">гиперссылка</a> вот так', parse_mode = types.ParseMode.HTML)
-            await message.answer(' html-2 потом жирным <strong><a href="tg://user?id={UsrInfo.id}">гиперссылка</a></strong>', parse_mode = types.ParseMode.HTML)
-            await message.answer(' html-3 отом жирным <a href="tg://user?id={UsrInfo.id}"><strong>гиперссылка</strong></a>', parse_mode = types.ParseMode.HTML)
+            #await message.answer("Hello со ссылкой, <a href=\"tg://user?id=1111111\">гиперссылка</a>", parse_mode=types.ParseMode.HTML)
+            #await message.answer(' html-0 вначале просто <a href=''tg://user?id=1111111''>гиперссылка</a> вот так', parse_mode = types.ParseMode.HTML)
+            #await message.answer(' html-1 вначале просто <a href="tg://user?id={UsrInfo.id}">гиперссылка</a> вот так', parse_mode = types.ParseMode.HTML)
+            #await message.answer(' html-2 потом жирным <strong><a href="tg://user?id={UsrInfo.id}">гиперссылка</a></strong>', parse_mode = types.ParseMode.HTML)
+            #await message.answer(' html-3 отом жирным <a href="tg://user?id={UsrInfo.id}"><strong>гиперссылка</strong></a>', parse_mode = types.ParseMode.HTML)
 
-            await message.answer(text=f' html-0 вначале просто <a href=\"tg://user?id=1111111\">гиперссылка</a> вот так', parse_mode = types.ParseMode.html)
-            # await message.answer(text=f' html-1 вначале просто <a href=\"tg://user?id={UsrInfo.id}\">гиперссылка</a> вот так', parse_mode = types.ParseMode.html)
-            # await message.answer(text=f' html-2 потом жирным <strong><a href="tg://user?id={UsrInfo.id}">гиперссылка</a></strong>', parse_mode = types.ParseMode.html)
-            # await message.answer(text=f' html-3 отом жирным <a href="tg://user?id={UsrInfo.id}"><strong>гиперссылка</strong></a>', parse_mode = types.ParseMode.html)
+            #await message.answer(text=f' html-0 вначале просто <a href=\"tg://user?id=1111111\">гиперссылка</a> вот так', parse_mode = types.ParseMode.html)
+            await message.answer(text=f' html-1 вначале просто <a href="tg://user?id={UsrInfo.id}">гиперссылка</a> вот так', parse_mode = types.ParseMode.HTML)
+            await message.answer(text=f' html-2 потом жирным <strong><a href="tg://user?id={UsrInfo.id}">гиперссылка</a></strong>', parse_mode = types.ParseMode.HTML)
+            await message.answer(text=f' html-3 отом жирным <a href="tg://user?id={UsrInfo.id}"><strong>гиперссылка</strong></a>', parse_mode = types.ParseMode.HTML)
             textOfForvardObiavy = '#ВАКАНСИЯ от @' + str(message.forward_from.username) + '\n\n' + message.text 
             textOfForvardObiavy = textOfForvardObiavy.replace("_", "\_")
             await message.answer(text=f'Итого получаем следующий текст:\n\n{textOfForvardObiavy}', parse_mode='Markdown')
