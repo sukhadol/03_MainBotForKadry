447aaac30e7450ec270ce8a2d041e62dbd478e1c
George Sukhadolsky <sukhadol@yandex.ru>
2021-10-19 15:00:19 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -325,7 +325,6 @@ async def strange_txt(message: types.Message):
             UsrInfo = message.forward_from
             await message.answer("Id: " + str(UsrInfo.id) + "\nFirst Name: " + str(UsrInfo.first_name) + "\nLast Name: " + str(UsrInfo.last_name) +
                             "\nUsername: @" + str(UsrInfo.username))
-            await message.answer(' у него (UsrInfo.mention_markdown)=' + str(UsrInfo.mention_markdown))
             await message.answer(' у него (UsrInfo.name)=' + str(UsrInfo.name))
 
             # await message.answer(text=f'сообщение от автора :\n\n@{message.forward_from.username}', parse_mode='Markdown')
